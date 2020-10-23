e6186fac01958b0e25fc767c5da81bf0f9447883
Michał Czekański <michal.cz2202@gmail.com>
2020-10-24 01:03:39 +0200

    Add constructor and fields in Note class

--- app/src/main/java/com/crimsonbeet/notes/models/Note.java
+++ app/src/main/java/com/crimsonbeet/notes/models/Note.java
@@ -1,5 +1,14 @@
 package com.crimsonbeet.notes.models;
 
 public class Note {
+    private int id;
+    private String title;
+    private String content;
 
+
+    public Note(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
 }
