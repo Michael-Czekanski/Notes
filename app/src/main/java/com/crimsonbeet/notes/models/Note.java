aa279b625556a4c7a25d32af76722a59da51f5a9
Michał Czekański <michal.cz2202@gmail.com>
2020-10-24 01:04:51 +0200

    Add getters and setters in Note class

--- app/src/main/java/com/crimsonbeet/notes/models/Note.java
+++ app/src/main/java/com/crimsonbeet/notes/models/Note.java
@@ -11,4 +11,24 @@ public class Note {
         this.title = title;
         this.content = content;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
