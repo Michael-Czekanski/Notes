7aab05e8e44ed49302725f030ba3860ef8e2365d
Michał Czekański <michal.cz2202@gmail.com>
2020-10-24 19:31:21 +0200

    Add setNoteTitle() in NoteActivity

--- app/src/main/java/com/crimsonbeet/notes/NoteActivity.java
+++ app/src/main/java/com/crimsonbeet/notes/NoteActivity.java
@@ -1,14 +1,28 @@
 package com.crimsonbeet.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NoteActivity extends AppCompatActivity {
 
+    private TextView textViewNoteTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        Intent intent = getIntent();
+        textViewNoteTitle = findViewById(R.id.textView_noteTitle);
+
+        setNoteTitle(intent);
+    }
+
+    private void setNoteTitle(Intent intent) {
+        String noteTitle = intent.getStringExtra(MainActivity.NOTE_TITLE);
+        textViewNoteTitle.setText(noteTitle);
     }
 }
