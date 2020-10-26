c0c7850ad63c6a59ac7307fc96e3aea70eabb368
Michał Czekański <michal.cz2202@gmail.com>
2020-10-26 20:47:37 +0100

    Add JsonManagerTest.readNoteFromJson()

--- app/src/test/java/com/crimsonbeet/notes/utils/JsonManagerTest.java
+++ app/src/test/java/com/crimsonbeet/notes/utils/JsonManagerTest.java
@@ -1,4 +1,68 @@
 package com.crimsonbeet.notes.utils;
 
+import com.crimsonbeet.notes.models.Note;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JsonManagerTest {
+
+
+    private boolean deleteNoteJsonFile(String filename) {
+        File file = new File(filename);
+        return file.delete();
+    }
+
+    /**
+     * Creates Json file
+     *
+     * @return Created Json filename
+     */
+    private String createNoteJsonFile(int noteId, String noteTitle, String noteContent) throws IOException {
+        String filename = "note" + noteId + ".json";
+        String jsonString = String.format("{id:%d,title:\"%s\",content:\"%s\"}",
+                noteId, noteTitle, noteContent);
+        File file = new File(filename);
+
+        if (file.exists()) {
+            if (!deleteNoteJsonFile(filename)) {
+                throw new IOException("File exists already and can not delete it.");
+            }
+        }
+
+        if (file.createNewFile()) {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(jsonString);
+            fileWriter.close();
+        } else {
+            throw new IOException();
+        }
+
+        return filename;
+    }
+
+    @Test
+    public void readNoteFromJson() throws IOException {
+        JsonManager jsonManager = new JsonManager();
+
+        int expectedNoteId = 1;
+        String expectedNoteTitle = "Title";
+        String expectedNoteContent = "Content";
+        // Create file
+        String jsonFilename = createNoteJsonFile(expectedNoteId, expectedNoteTitle, expectedNoteContent);
+        // Read from file
+        Note actualNote = jsonManager.readNoteFromJson(expectedNoteId);
+        // Delete file
+        if (!deleteNoteJsonFile(jsonFilename)) {
+            throw new IOException("Can not delete file");
+        }
+
+        Assert.assertEquals(expectedNoteId, actualNote.getId());
+        Assert.assertEquals(expectedNoteTitle, actualNote.getTitle());
+        Assert.assertEquals(expectedNoteContent, actualNote.getContent());
+    }
 }
