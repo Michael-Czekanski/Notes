2e20caa27b6dbf2c5adec92e2d6b18e876c317c6
Michał Czekański <michal.cz2202@gmail.com>
2020-10-26 19:16:06 +0100

    Make methods private and remove their tests - JsonManager class
    These methods should not be public.

--- app/src/main/java/com/crimsonbeet/notes/utils/JsonManager.java
+++ app/src/main/java/com/crimsonbeet/notes/utils/JsonManager.java
@@ -1,41 +1,20 @@
 package com.crimsonbeet.notes.utils;
 
-import androidx.annotation.Nullable;
-
 import com.crimsonbeet.notes.models.Note;
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class JsonManager {
-    public static String noteToJsonString(Note note) {
+    private static String noteToJsonString(Note note) {
         Gson gson = new Gson();
         return gson.toJson(note);
     }
 
-    public static String getJsonFilename(int nodeId) {
+    private static String getJsonFilename(int nodeId) {
         return "note" + nodeId + ".json";
-    }
-
-
-    /**
-     * Tries to create note object from given json string
-     *
-     * @param json Note object written as json
-     * @return Note object or null
-     */
-    @Nullable
-    public static Note noteFromJsonString(String json) {
-        Gson gson = new Gson();
-
-        try {
-            return gson.fromJson(json, Note.class);
-        } catch (JsonSyntaxException e) {
-            return null;
-        }
     }
 
     private static Scanner openJsonFile(String filePath) throws FileNotFoundException {
--- app/src/test/java/com/crimsonbeet/notes/utils/JsonManagerTest.java
+++ app/src/test/java/com/crimsonbeet/notes/utils/JsonManagerTest.java
@@ -1,45 +1,4 @@
 package com.crimsonbeet.notes.utils;
 
-import com.crimsonbeet.notes.models.Note;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 public class JsonManagerTest {
-
-
-    @Test
-    public void noteToJsonString() {
-        Note note = new Note(1, "Test", "Content");
-
-        String expectedString = "{\"id\":1,\"title\":\"Test\",\"content\":\"Content\"}";
-        String actualString = JsonManager.noteToJsonString(note);
-
-        assertEquals(expectedString, actualString);
-    }
-
-    @Test
-    public void getJsonFilename() {
-        int noteId = 1;
-
-        String expectedFilename = "note1.json";
-        String actualFilename = JsonManager.getJsonFilename(noteId);
-
-        assertEquals(expectedFilename, actualFilename);
-    }
-
-    @Test
-    public void noteFromJsonString_CorrectString() {
-        String json = "{\"id\":1,\"title\":\"Test\",\"content\":\"Content\"}";
-
-        Note expectedNote = new Note(1, "Test", "Content");
-        Note actualNote = JsonManager.noteFromJsonString(json);
-
-        assertNotNull(actualNote);
-        assertEquals(expectedNote.getId(), actualNote.getId());
-        assertEquals(expectedNote.getTitle(), actualNote.getTitle());
-        assertEquals(expectedNote.getContent(), actualNote.getContent());
-    }
 }
