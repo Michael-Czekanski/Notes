a40252357d1c48b3ff0d97558ac2a18a02f2985f
miccze <miccze7@st.amu.edu.pl>
2020-12-05 17:56:43 +0100

    Add methods in JsonManager to manage EncryptedNote objects

--- app/src/main/java/com/crimsonbeet/notes/utils/JsonManager.java
+++ app/src/main/java/com/crimsonbeet/notes/utils/JsonManager.java
@@ -1,5 +1,6 @@
 package com.crimsonbeet.notes.utils;
 
+import com.crimsonbeet.notes.models.EncryptedNote;
 import com.crimsonbeet.notes.models.Note;
 import com.google.gson.Gson;
 
@@ -29,14 +30,40 @@ public class JsonManager {
     private boolean deserializationSuccess(Note deserializedNote){
         boolean success = true;
 
-        if(deserializedNote.getTitle() == null){
+        if (deserializedNote.getTitle() == null) {
             success = false;
         }
-        if(deserializedNote.getContent() == null){
+        if (deserializedNote.getContent() == null) {
             success = false;
         }
         return success;
     }
+
+    /**
+     * Checks if encrypted note deserialized successfully
+     *
+     * @param deserializedEncryptedNote
+     * @return True or false
+     */
+    private boolean deserializationSuccess(EncryptedNote deserializedEncryptedNote) {
+        boolean success = true;
+
+        if (deserializedEncryptedNote.getEncryptedTitle() == null) {
+            success = false;
+        }
+        if (deserializedEncryptedNote.getEncryptedContent() == null) {
+            success = false;
+        }
+        if (deserializedEncryptedNote.getEncryptedNonce() == null) {
+            success = false;
+        }
+        if (deserializedEncryptedNote.getSalt() == null) {
+            success = false;
+        }
+
+        return success;
+    }
+
 
     private FileReader openJsonFile(String filePath) throws FileNotFoundException {
         File jsonFile = new File(filePath);
@@ -80,14 +107,77 @@ public class JsonManager {
         }
     }
 
-    public boolean deleteNoteJsonFile(int noteId){
+    public boolean deleteNoteJsonFile(int noteId) {
         File file = new File(getJsonFilename(noteId));
         return file.delete();
     }
 
-    public boolean deleteNoteJsonFile(Note note){
+    public boolean deleteNoteJsonFile(Note note) {
         File file = new File(getJsonFilename(note.getId()));
         return file.delete();
     }
+
+    public boolean deleteEncryptedNoteJsonFile(EncryptedNote encryptedNote) {
+        File file = new File(getJsonFilename(encryptedNote.getId()));
+        return file.delete();
+    }
+
+    public boolean deleteEncryptedNoteJsonFile(int noteId) {
+        File file = new File(getJsonFilename(noteId));
+        return file.delete();
+    }
+
+    /**
+     * Makes json string from encrypted note.
+     *
+     * @param encryptedNote Note to convert to json string.
+     * @return Json string.
+     */
+    public String encryptedNoteToJsonString(EncryptedNote encryptedNote) {
+        return gson.toJson(encryptedNote);
+    }
+
+    /**
+     * Writes encrypted note to json file.
+     *
+     * @param encryptedNote Note to write to json.
+     * @return Json filename.
+     * @throws IOException When there was error writing encrypted note to json file.
+     */
+    public String writeEncryptedNoteToJson(EncryptedNote encryptedNote) throws IOException {
+        String filename = getJsonFilename(encryptedNote.getId());
+
+        File file = new File(filename);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(encryptedNoteToJsonString(encryptedNote));
+            fileWriter.close();
+            return filename;
+        } catch (IOException e) {
+            throw new IOException("Error writing encrypted note to json file.");
+        }
+    }
+
+    /**
+     * Reads encrypted note from json file.
+     *
+     * @param noteId Note to read.
+     * @return Deserialized encrypted note or null if deserialization was not successful.
+     * @throws FileNotFoundException If there was no json file corresponding to given note id.
+     * @throws IOException           If there was problem with closing file reader.
+     */
+    public EncryptedNote readEncryptedNoteFromJson(int noteId) throws IOException {
+        String filename = getJsonFilename(noteId);
+        FileReader reader = openJsonFile(filename);
+
+        EncryptedNote encryptedNote = gson.fromJson(reader, EncryptedNote.class);
+        reader.close();
+        if (deserializationSuccess(encryptedNote)) {
+            return encryptedNote;
+        }
+        return null;
+    }
+
+
 
 }
